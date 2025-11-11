7ea6ddb9151474c9cb1464ab6f1911fcffc06c26
Pedro Gonzalezm <gpedro.17.3f@gmail.com>
2025-11-10 21:56:21 -0600

    Se ordnan de forma asc po fecha de creacion los BASELINE

--- app/src/main/java/com/example/etic/data/local/dao/LineaBaseDao.kt
+++ app/src/main/java/com/example/etic/data/local/dao/LineaBaseDao.kt
@@ -7,15 +7,15 @@ import com.example.etic.data.local.entities.LineaBase
 @Dao
 interface LineaBaseDao {
 
-    @Query("SELECT * FROM linea_base")
+    @Query("SELECT * FROM linea_base ORDER BY Fecha_Creacion ASC")
     suspend fun getAll(): List<LineaBase>
 
-    @Query("SELECT * FROM linea_base WHERE Estatus = 'Activo'")
+    @Query("SELECT * FROM linea_base WHERE Estatus = 'Activo' ORDER BY Fecha_Creacion ASC")
     suspend fun getAllActivos(): List<LineaBase>
 
-    @Query("SELECT * FROM linea_base WHERE Id_Inspeccion = :idInspeccion")
+    @Query("SELECT * FROM linea_base WHERE Id_Inspeccion = :idInspeccion ORDER BY Fecha_Creacion ASC")
     suspend fun getByInspeccion(idInspeccion: String): List<LineaBase>
 
-    @Query("SELECT * FROM linea_base WHERE Id_Inspeccion = :idInspeccion AND Estatus = 'Activo'")
+    @Query("SELECT * FROM linea_base WHERE Id_Inspeccion = :idInspeccion AND Estatus = 'Activo' ORDER BY Fecha_Creacion ASC")
     suspend fun getByInspeccionActivos(idInspeccion: String): List<LineaBase>
 }
